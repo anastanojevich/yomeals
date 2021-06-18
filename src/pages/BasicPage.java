16d3f17f65f76a32e12440e5c9da4a169b1fe87f
Ana Stanojevic <ana.stanojevich@gmail.com>
2021-06-18 19:11:47 +0200

    Change BasicPage

--- src/pages/BasicPage.java
+++ src/pages/BasicPage.java
@@ -1,5 +1,28 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
 public abstract class BasicPage {
 
+	protected WebDriver driver;
+	protected JavascriptExecutor js;
+	
+	public BasicPage(WebDriver driver, JavascriptExecutor js) {
+		this.driver = driver;
+		this.js = js;
+	}
+	
+	public boolean elementExist(By by) {
+		boolean exist = true;
+		
+		try {
+			driver.findElement(by);
+		} catch (Exception e) {
+			exist = false;
+		}
+		return exist;		
+	}
+	
 }

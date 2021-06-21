d124233c08cd75871ebf143a9e7f1c339d73f88f
Ana Stanojevic <ana.stanojevich@gmail.com>
2021-06-21 20:25:43 +0200

    Modify BasicPage

--- src/pages/BasicPage.java
+++ src/pages/BasicPage.java
@@ -3,15 +3,18 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class BasicPage {
 
 	protected WebDriver driver;
 	protected JavascriptExecutor js;
+	protected WebDriverWait wait;
 	
-	public BasicPage(WebDriver driver, JavascriptExecutor js) {
+	public BasicPage(WebDriver driver, JavascriptExecutor js, WebDriverWait wait) {
 		this.driver = driver;
 		this.js = js;
+		this.wait = wait;
 	}
 	
 	public boolean elementExist(By by) {

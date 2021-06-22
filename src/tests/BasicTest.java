8b1f24c79abfd414af52f3a23b5f12abea4420a3
Ana Stanojevic <ana.stanojevich@gmail.com>
2021-06-23 01:41:54 +0200

    Create BasicTest

---
+++ src/tests/BasicTest.java
@@ -0,0 +1,56 @@
+package tests;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+
+import pages.AuthPage;
+import pages.CartSummaryPage;
+import pages.LocationPopupPage;
+import pages.LoginPage;
+import pages.MealPage;
+import pages.NotificationSystemPage;
+import pages.ProfilePage;
+
+
+public abstract class BasicTest {
+
+	protected WebDriver driver;
+	protected JavascriptExecutor js;
+	protected WebDriverWait wait;
+	protected LoginPage loginPage;
+	protected LocationPopupPage locationPage;
+	protected NotificationSystemPage notificationPage;
+	protected ProfilePage profilePage;
+	protected AuthPage authPage;
+	protected MealPage mealPage;
+	protected CartSummaryPage cartPage;
+	protected String baseURL = "http://demo.yo-meals.com/";
+	protected String email = "customer@dummyid.com";
+	protected String password = "12345678a";
+	
+	@BeforeMethod
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		
+		loginPage = new LoginPage(driver, js, wait);
+		locationPage = new LocationPopupPage(driver, js, wait);
+		notificationPage = new NotificationSystemPage(driver, js, wait);
+		profilePage = new ProfilePage(driver, js, wait);
+		authPage = new AuthPage(driver, js, wait);
+		mealPage = new MealPage(driver, js, wait);
+		cartPage = new CartSummaryPage(driver, js, wait);
+	}
+	
+	
+	
+	
+}

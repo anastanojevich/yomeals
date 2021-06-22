525e610d99d374d0a11e561dd4a384c73d1c78cf
Ana Stanojevic <ana.stanojevich@gmail.com>
2021-06-22 21:10:05 +0200

    Create MealPage

---
+++ src/pages/MealPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MealPage extends BasicPage{
+
+	public MealPage(WebDriver driver, JavascriptExecutor js, WebDriverWait wait) {
+		super(driver, js, wait);
+	}
+
+}

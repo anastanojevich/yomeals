a47b51a08384cbc53022302236a40007c77fbe6b
Ana Stanojevic <ana.stanojevich@gmail.com>
2021-06-24 01:55:20 +0200

    Modify ProfileTest

--- src/tests/ProfileTest.java
+++ src/tests/ProfileTest.java
@@ -7,13 +7,18 @@ public class ProfileTest extends BasicTest{
 
 	@Test
 	public void editProfileTest() throws InterruptedException {
-		driver.get("http://demo.yo-meals.com/guest-user/login-form");
+		driver.get(this.baseURL + "guest-user/login-form");
 		locationPage.getLocationCloseBtn().click();
 		loginPage.submitLoginInfo("customer@dummyid.com", "12345678a");
 		Thread.sleep(2000);
 		Assert.assertEquals(notificationPage.getMessageText(), "Login Successfull");
 		
-		driver.get("http://demo.yo-meals.com/guest-user/login-form");
+		driver.get(this.baseURL + "member/profile");
+		profilePage.profileUpdate("Jane", "Doe", "Cool Street 22", "222-333", "111", "Wonderland", "Happiness", "Joy");
+		Thread.sleep(2000);
+		Assert.assertEquals(notificationPage.getMessageText(), "Setup Successfull");
+		
+		
 	}
 	
 }
